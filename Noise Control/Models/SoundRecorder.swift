6d2aa83b5029b62b2695ad5fd67c2aaae864a864
Piotr Narecki <nareckipiotraleksander@gmail.com>
2023-04-03 20:14:38 +0200

    MVC basic applied

---
+++ Noise Control/Models/SoundRecorder.swift
@@ -0,0 +1,23 @@
+//
+//  SoundRecorder.swift
+//  Noise Control
+//
+//  Created by Piotr Narecki on 03/04/2023.
+//
+
+import Foundation
+
+class SoundRecorder{
+    
+    var frequency: Float=0.0
+    
+
+    func recordSound(){
+        
+        frequency=420.69
+        
+        print("generating \(String(frequency))")
+        
+    }
+    
+}
